9a7377d0923265384dc3c5e6287dc374f10e72b6
Matthew Andrews <matthew@andrews.eu.com>
2013-04-25 08:45:14 +0100

    Add note about feature detection

--- client.js
+++ client.js
@@ -1,3 +1,9 @@
+/**
+ * localstorage-boot Entry Point
+ *
+ * @author Matt Andrews <matt@mattandre.ws>
+ * @copyright The Financial Times
+ */
 var upgrade = require('./lib/resources.json/client');
 
 module.exports = upgrade;
